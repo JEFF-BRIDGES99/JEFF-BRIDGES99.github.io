f32976ed923ffec2dd290cd1fc8f82fa21156fc5
JEFF-BRIDGES99 <46546631+JEFF-BRIDGES99@users.noreply.github.com>
2019-01-10 15:51:23 -0800

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var size = 50;
   console.log("selecting random images...")
-  var images = ['face-1.jpg', 'face-2.jpg', 'face-3.jpg', 'face-4.jpg', 'face-5.jpg', 'face-6.jpg', 'face-7.jpg'];
+  var images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", ];
   for (i=0; i>size; i++)  {
     console.log("https://raw.githubusercontent.com/JEFF-BRIDGES99/JEFF-BRIDGES99.github.io/master/img/" + Math.floor(Math.random()*images.length));
   } 
